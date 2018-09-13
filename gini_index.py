8012247e428c8b4107e230d6648407442138e5e5
Hectodux <40269428+Hectodux@users.noreply.github.com>
2018-09-13 18:23:06 +0200

    Add files via upload

---
+++ gini_index.py
@@ -0,0 +1,22 @@
+def calculate_gini_index(groups, classes):
+    n_instances = float(sum([len(group) for group in groups]))
+    gini = 0.0
+    for group in groups:
+        size = float(len(group))
+        if size == 0:
+            continue
+        score = 0.0
+        for class_val in classes:
+            p = [row[-1] for row in group].count(class_val) / size
+            score += p * p
+        gini += (1.0 - score) * (size / n_instances)
+    return gini
+
+
+def main():
+    print(calculate_gini_index([[[1, 1], [1, 0]], [[1, 1], [1, 0]]], [0, 1]))
+    print(calculate_gini_index([[[1, 0], [1, 0]], [[1, 1], [1, 1]]], [0, 1]))
+
+
+if __name__ == "__main__":
+    main()
